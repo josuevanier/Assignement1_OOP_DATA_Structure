fcb5ab83604f3abb75b1ecb2c010b839d4a68d2b
josuevanier <150824779+josuevanier@users.noreply.github.com>
2024-03-09 09:24:31 -0500

    Find the problem in my code rember that the index issue was because I decicide dto say at the index + 1 which does not make sense at all . Second of all each time we call the remove or add it create a new memory address so we must store that value somewhere
    - Next step exceptions

--- src/main/java/org/example/Question1.java
+++ src/main/java/org/example/Question1.java
@@ -4,14 +4,33 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Question1 {
+    static Scanner sc = new Scanner(System.in);
+    static boolean isExit = false;
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        int[] array = {1,2,2,2,2,4,4,5,6};
-        Arrays.sort(array);
+        int[] array = getInputsArrays();
 
+        String msgMenu = """
+         * You must have an array in order to have these features        
+         * If you want to add an element to an array enter: 2
+         * If you want to sort an array enter: 3
+         * If you want to remove an array enter: 4
+         
+         * If you want to find all the repeated elements(elements are automaticly sorted) 
+         and the one that is repeated the most enter : 5
+    
+         * If you want to display display enter: 6
+         * If you want to see what the number you will enter does, enter: 7
+         * If you want to exit enter: 8 
+                """;
 
-
-        findRepeatedElements(array);
+        while (!isExit){
+            System.out.println("What feature would you like . Note it can only be one " +
+                    "of the feature stated earlier: ");
+            int num = sc.nextInt();;
+            array = menu(num,array);
+        }
     }
 
 
@@ -20,19 +39,15 @@ public class Question1 {
 
 
     public static  int[] getInputsArrays(){
-        Scanner sc = new Scanner(System.in);
-
         System.out.println("Hi ! How many elements you would like to enter : ");
 
         int numberOfElements = sc.nextInt();
-
         int index = 0;
-
         int count = 0;
+
         int[] arrays = new int[numberOfElements];
         while(index < numberOfElements){
             System.out.println("Enter your " + count + ": ");
-
             arrays[count] = sc.nextInt();
             count++;
             index++ ;
@@ -40,34 +55,103 @@ public class Question1 {
         return arrays;
     }
 
-    public static  int[] addElement(int[] arrays, int num){
+    public static int[] addElement(int[] arrays, int num) {
         int[] addedElements = new int[arrays.length + 1];
-
-        for(int i = 0;  i < arrays.length; i++){
-                addedElements[addedElements.length - 1 ] = num;
-                addedElements[i] =  arrays[i];
+        for (int i = 0; i < arrays.length; i++) {
+            addedElements[addedElements.length - 1] = num;
+            addedElements[i] = arrays[i];
         }
         return addedElements;
     }
     public static void findRepeatedElements(int[] arrays){
+        int max = Integer.MIN_VALUE;
         Arrays.sort(arrays);
         int count = 0;
-        int currentElement;
+        int currentElement = 0;
         int index = 0;
-
         for(int i = index; i <= arrays.length - 1; i++){
-
-            for(int j = i; j < arrays.length; j++) {
-                if (arrays[i] == arrays[j]) {
+            for(int j = i  ; j < arrays.length; j++) {
+                if ( (arrays[i] == arrays[j])) {
                     index++;
                     count++;
                 }
             }
-                System.out.print("The element " + arrays[i] + " is repeated " + count + " time");
-            i = index;
-                count = 0;
-            System.out.println(" ");
-
+            if(count >= 1 ){ System.out.print("The element " + arrays[i] + " " +
+                    "is repeated " + count + " time");
             }
+            i = index;
+            if( count > max && count > 1){
+                max = count;
+                currentElement = arrays[i];
+            }
+            System.out.print("\n");
+            }
+        if( max <= 1){
+            System.out.println("No occurence ! ");
+        }else {
+            System.out.println("The element " + currentElement + " is repeated the most " + max);
+        }
+    }
+
+    public static  int[] removeElement(int[] arrays, int index){
+        if(index > arrays.length){
+                throw new ArrayIndexOutOfBoundsException("This index is bigger than the array length it self " +
+                        "(" + arrays.length + ")" + " Please enter a valid index");
+        }
+        int[] removedArray =  new int[arrays.length - 1];
+        if(index == arrays.length - 1){
+            for(int t = 0; t < removedArray.length; t++){
+                removedArray[t] = arrays[t];
+            }
+        }
+
+        for(int i = 0; i < index; i++){
+            removedArray[i] = arrays[i];
+        }
+        for(int j =  (index + 1); j <= removedArray.length; j++){
+            removedArray[j - 1] = arrays[j];
+        }
+        return removedArray;
+    }
+    public static void display(int[] arrays){
+        System.out.println(Arrays.toString(arrays));
+    }
+    public static void sortArray(int[] array){
+        Arrays.sort(array);
+    }
+    public static int[] menu(int num,int[] arr){
+
+            switch (num) {
+                case 1:
+                    System.out.println("Integer value you want to add ?");
+
+                    arr = addElement(arr, sc.nextInt());
+                   break;
+                case 2:
+                    sortArray(arr);
+                    break;
+                case 3:
+                    System.out.println("Index to remove the element:");
+                    arr = removeElement(arr, sc.nextInt());
+                    break;
+                case 4:
+                    findRepeatedElements(arr);
+
+                case 5:
+                   display(arr);
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    isExit = wantExit();
+                    break;
+                default:
+                    System.out.println("Invalid option");
+                    break;
+            }
+            return arr;
+    }
+    public static boolean wantExit(){
+        return true;
     }
 }
