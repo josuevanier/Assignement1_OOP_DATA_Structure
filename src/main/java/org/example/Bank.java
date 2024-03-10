2e597d7153dee491577240f61c95356331f23718
josuevanier <150824779+josuevanier@users.noreply.github.com>
2024-03-10 15:51:06 -0400

    Question 4 : Bank final version with all method called

--- src/main/java/org/example/Bank.java
+++ src/main/java/org/example/Bank.java
@@ -3,6 +3,10 @@ package org.example;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
+/**
+ * Contains the info of a bank
+ * @author  Joseph Josue Forestal
+ */
 public class Bank {
     String name;
     String branch;
@@ -10,6 +14,14 @@ public class Bank {
     String address;
     int noOfCustomers;
 
+    /***
+     * Constructor
+     * @param name name of the bank
+     * @param branch branch of the bank
+     * @param revenue revenue of the bank
+     * @param address address of the bank
+     * @param noOfCustomers number of customers of the bank
+     */
     public Bank(String name, String branch, double revenue, String address, int noOfCustomers) {
         this.name = name;
         this.branch = branch;
@@ -18,42 +30,78 @@ public class Bank {
         this.noOfCustomers = noOfCustomers;
     }
 
+    /**
+     * Get the name of the bank
+     * @return the name of the bank
+     */
     public String getName() {
         return name;
     }
-
+    /**
+     * Get the branch of the bank
+     * @return the branch of the bank
+     */
     public String getBranch() {
         return branch;
     }
-
+    /**
+     * Get the revenue of the bank
+     * @return the revenue of the bank
+     */
     public double getRevenue() {
         return revenue;
     }
-
+    /**
+     * Get the address of the bank
+     * @return the address of the bank
+     */
     public String getAddress() {
         return address;
     }
-
+    /**
+     * Get the number of customers of the bank
+     * @return the number of customers of the bank
+     */
     public int getNoOfCustomers() {
         return noOfCustomers;
     }
 
+    /***
+     * Set te name of the bank
+     * @param name the name that is being set
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /***
+     * Set the branch of the bank
+     * @param branch   that is being set
+     */
     public void setBranch(String branch) {
         this.branch = branch;
     }
 
+    /***
+     * Set the revenue of the bank
+     * @param revenue the name that is being set
+     */
     public void setRevenue(double revenue) {
         this.revenue = revenue;
     }
 
+    /***
+     * Set the address of the bank
+     * @param address the name that is being set
+     */
     public void setAddress(String address) {
         this.address = address;
     }
 
+    /***
+     * Set the number of customers of the bank
+     * @param noOfCustomers  the number of customers that is being set
+     */
     public void setNoOfCustomers(int noOfCustomers) {
         this.noOfCustomers = noOfCustomers;
     }
@@ -76,10 +124,18 @@ public class Bank {
         banks.add(new Bank("RBC", "CDG", 500000, "5043", 215));
 
         displayBankThatMakeMoreRevenue(banks);
+
         displayNumberOfCustomerOfTheBanks(banks);
+
+        countBank(banks);
 
     }
 
+    /***
+     * Find Bank of a particular branch based of the amount of money
+     * @param banks the bank
+     * @param bankName the bank name
+     */
     public static void findBankOfParticularBranch(ArrayList<Bank> banks, String bankName) {
         if (bankName.equalsIgnoreCase("Td") || bankName.equalsIgnoreCase("Rbc")) {
             for (Bank bank : banks) {
@@ -91,6 +147,10 @@ public class Bank {
         }
     }
 
+    /***
+     * Count the number of banks of a bank
+     * @param banks the bank that is being count
+     */
     public static void countBank(ArrayList<Bank> banks) {
         int countTD = 0;
         int countRbc = 0;
@@ -98,8 +158,15 @@ public class Bank {
             if (bank.getName().equalsIgnoreCase("Td")) countTD++;
             else if (bank.getName().equalsIgnoreCase("RBC")) countRbc++;
         }
+        System.out.println("Td has " + countTD + " Banks.");
+
+        System.out.println("Rbc has " + countRbc + " Banks.");
     }
 
+    /***
+     * Display the bank that make more revenue
+     * @param banks the banks that we counting
+     */
     public static void displayBankThatMakeMoreRevenue(ArrayList<Bank> banks) {
         int sumRBC = 0;
         int sumTD = 0;
@@ -116,6 +183,10 @@ public class Bank {
         else System.out.println("They make the same amount of revenue " + sumTD + "$");
     }
 
+    /***
+     * Display the number of Customer of a bank
+     * @param banks banks we want to display
+     */
     public static void displayNumberOfCustomerOfTheBanks(ArrayList<Bank> banks) {
         int sumCustomerRbc = 0;
         int sumCustomerTD = 0;
@@ -128,6 +199,5 @@ public class Bank {
         }
         System.out.println("Bank RBC  have a total of " + sumCustomerRbc );
         System.out.println("Bank TD have a total of " + sumCustomerTD);
-
     }
 }
