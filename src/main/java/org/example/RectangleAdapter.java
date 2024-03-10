86cf937dbd17893e130a0de6c6132e2aa00e5e98
josuevanier <150824779+josuevanier@users.noreply.github.com>
2024-03-10 15:36:16 -0400

    Rectangle adpter with the main method that prints the value : Final

--- src/main/java/org/example/RectangleAdapter.java
+++ src/main/java/org/example/RectangleAdapter.java
@@ -1,19 +1,33 @@
 package org.example;
 
+/**
+ * Contains the info of Rectangle adapter
+ * @author  Joseph JoSUE fORESTAL
+ */
 public class RectangleAdapter{
 
     public Rectangle rectangle;
 
+    /***
+     * Constructor
+     * @param rectangle take  an interface typf of parameter
+     */
     public RectangleAdapter(Rectangle rectangle) {
         this.rectangle = rectangle;
     }
 
+    /**
+     * Set the side of Rectangle adapter
+     * @param side the side that is being set
+     */
     public void setSide(int side){
         rectangle.setLength(side);
         rectangle.setWidth(side);
     }
 
-
+    /***
+     * Print the area of a square (Rectangle adapter)
+     */
     public void printAreaOfSquare(){
         rectangle.printAreaOfRectangle();
     }
@@ -21,7 +35,12 @@ public class RectangleAdapter{
     public static void main(String[] args) {
         RectangleAdapter adapter = new RectangleAdapter(new TennisCourt());
         adapter.setSide(4);
-
         adapter.printAreaOfSquare();
+
+        ChessBoard ch = new ChessBoard(2);
+        ch.printAreaOfSquare();
+
+        TennisCourt tenis = new TennisCourt(3,4);
+        tenis.printAreaOfRectangle();
     }
 }
