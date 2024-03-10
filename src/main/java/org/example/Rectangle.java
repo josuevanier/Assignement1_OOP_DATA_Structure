4d61d0a78b380b48bd8bb0dccd92405fdccc0cba
josuevanier <150824779+josuevanier@users.noreply.github.com>
2024-03-10 15:41:02 -0400

    Interface Rectangle : Final version !

--- src/main/java/org/example/Rectangle.java
+++ src/main/java/org/example/Rectangle.java
@@ -1,5 +1,9 @@
 package org.example;
 
+/**
+ * Contains the method of Rectangle
+ * @author  Joseph Josue Forestal
+ */
 public interface Rectangle {
     void setLength(int side);
     void setWidth(int side);
