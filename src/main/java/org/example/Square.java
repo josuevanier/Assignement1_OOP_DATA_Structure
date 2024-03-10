fdfc8398f6dff573c01ae30daaea36b0b5eeeed0
josuevanier <150824779+josuevanier@users.noreply.github.com>
2024-03-10 15:32:41 -0400

    Square interface final version

--- src/main/java/org/example/Square.java
+++ src/main/java/org/example/Square.java
@@ -1,7 +1,10 @@
 package org.example;
 
+/**
+ * Contains the info of interface square
+ * @author  Joseph Josue Forestal
+ */
 public interface Square {
      void setSide(int side);
-
      void printAreaOfSquare();
 }
